06bc9b68b433419567af23d723d8ccab98cc54b7
Kshitij <89059032+kshitij-shinde@users.noreply.github.com>
2022-10-23 10:57:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "/src/Header.jsx";
+import Footer from "/src/Footer.jsx";
+import Note from "/src/Note.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
